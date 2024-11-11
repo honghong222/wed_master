d90fca8a811a92c0200197d5a27b825e7208806a
pc <pc@email.com>
2024-11-11 17:45:49 +0900

    저장

---
+++ js/exe3.js
@@ -0,0 +1,21 @@
+//exe.js
+//1. 태어난 년도 입력 -> 나이계산.
+//2. 직사각형의 너비 계산 -> 가로길이, 세로길이 입력.
+//3. 최고값, 최소값 ->입력숫자는 3개.
+
+let question=1;
+
+if(question==1){
+  //1번코드
+  let param1=prompt('태어난 년도 입력.')
+  var year = 2024
+  var asd
+  asd=year-param1+1
+  document.write(asd+'살입니다')
+}
+else if(question==2){
+  //2번
+}
+else{
+  //3번
+}
